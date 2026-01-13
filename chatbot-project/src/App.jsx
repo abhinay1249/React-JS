515ceaba03eb5d8e8f67adfcc6c94faa53647070
abhinay1249 <abhinaymarise@gmail.com>
2026-01-13 23:02:32 +0530

    React Setup

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,50 +1,9 @@
 import { useState, useEffect, useRef } from 'react'
 import './App.css'
-import RobotProfileImage from './assets/robot.png';
-import UserProfileImage from './assets/user.png';
 import { ChatInput } from './components/ChatInput';
 
  
-// ========================== Chat User Component ==========================
 
-function ChatUser({ message, sender }) {
-  // const message = props.message;
-  // const sender = props.sender;
-  // const {message,sender} = props;
-  /*
-  if(sender === "robot"){
-    return(
-      <div>
-        <img src="Images/robot.png" width="50"/>
-        {message};
-      </div>
-    )
-  }
-  */
-  return (
-    <div className={
-      sender === 'User'
-        ? 'chat-message-user'
-        : 'chat-message-robot'
-    }>
-      {sender === 'Robot' && (
-        <img
-          className="chat-message-profile"
-          src={RobotProfileImage}
-        />
-      )}
-      <div className="chat-message-text">
-        {message}
-      </div>
-      {sender === 'User' && (
-        <img
-          className="chat-message-profile"
-          src={UserProfileImage}
-        />
-      )}
-    </div>
-  );
-}
 // ====================== Chat Messages Component =============================
 
 function useAutoScroll(dependencies){
