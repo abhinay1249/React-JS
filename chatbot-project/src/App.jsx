06796a842b859d3cefbebc684e5d0144a820c403
abhinay1249 <abhinaymarise@gmail.com>
2026-01-13 23:08:50 +0530

    React Setup

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,44 +1,10 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState} from 'react'
 import './App.css'
 import { ChatInput } from './components/ChatInput';
-
+import ChatMessages from './components/ChatMessages';
  
 
-// ====================== Chat Messages Component =============================
 
-function useAutoScroll(dependencies){
-
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    const containerElem = containerRef.current
-    if (containerElem) {
-      containerElem.scrollTop = containerElem.scrollHeight;
-    }
-  },dependencies);
-  return containerRef;
-}
-function ChatMessages({ chatMessages}) {
-
-  const chatMessagesRef = useAutoScroll([chatMessages]);
-
-  return (
-    <div
-      className="chat-messages-container"
-      ref={chatMessagesRef}
-    >
-      {chatMessages.map((chatMessage) => {
-        return (
-          <ChatUser
-            message={chatMessage.message}
-            sender={chatMessage.sender}
-            key={chatMessage.id}
-          />
-        );
-      })}
-    </div>
-  );
-}
 
 function App(){
   const [chatMessages, setChatMessages] = useState([]);
---
+++ chatbot-project/src/components/ChatMessages.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef } from 'react'
+import { ChatUser } from './ChatUser';
+
+
+function useAutoScroll(dependencies){
+
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const containerElem = containerRef.current
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  },dependencies);
+  return containerRef;
+}
+
+function ChatMessages({ chatMessages}) {
+
+  const chatMessagesRef = useAutoScroll([chatMessages]);
+
+  return (
+    <div
+      className="chat-messages-container"
+      ref={chatMessagesRef}
+    >
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatUser
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            key={chatMessage.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default ChatMessages;
