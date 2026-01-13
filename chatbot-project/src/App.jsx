e2139ef6d47a33bdf1cadc10cf7fb81af758226e
abhinay1249 <abhinaymarise@gmail.com>
2026-01-13 22:59:11 +0530

    React Setup

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,81 +1,10 @@
 import { useState, useEffect, useRef } from 'react'
-import { Chatbot } from 'supersimpledev'
 import './App.css'
 import RobotProfileImage from './assets/robot.png';
 import UserProfileImage from './assets/user.png';
-import LoadingSpinner from './assets/loading-spinner.gif';
+import { ChatInput } from './components/ChatInput';
 
-
-
-function ChatInput({ chatMessages, setChatMessages }) {
-  const [inputText, setInputText] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
-  function saveInputText(event) {
-    setInputText(event.target.value);
-  }
-
-  async function sendMessage() {
-    if (isLoading || inputText === '') {
-      return;
-    }
-    setIsLoading(true);
-    const newChatMessages = [
-      ...chatMessages,
-      {
-        message: inputText,
-        sender: 'User',
-        id: crypto.randomUUID()
-      }
-    ];
-
-    setInputText('');
-    setChatMessages([
-      ...newChatMessages,
-      {
-        message: <img src={LoadingSpinner} className="loading-spinner"/>,
-        sender: 'Robot',
-        id: crypto.randomUUID()
-      }
-    ]);
-    const response = await (Chatbot.getResponseAsync(inputText));
-    setChatMessages([
-      ...newChatMessages,
-      {
-        message: response,
-        sender: 'Robot',
-        id: crypto.randomUUID()
-      }
-    ]);
-    setIsLoading(false);
-  }
-
-  function handleKeyDown(event) {
-    if (event.key === 'Enter') {
-      sendMessage();
-    } else if (event.key === 'Escape') {
-      setInputText('');
-    }
-  }
-
-  return (
-    <div className="input-area-container">
-      <input
-        className="input-area"
-        placeholder="Send a message to Chatbot"
-        size="30"
-        onChange={saveInputText}
-        onKeyDown={handleKeyDown}
-        value={inputText} // Controlled Input
-      />
-      <button
-        className="send-button"
-        onClick={sendMessage}
-      >Send</button>
-    </div>
-  );
-}
-
+ 
 // ========================== Chat User Component ==========================
 
 function ChatUser({ message, sender }) {
---
+++ chatbot-project/src/components/ChatInput.jsx
@@ -0,0 +1,74 @@
+import{ useState } from 'react';
+import { Chatbot } from 'supersimpledev'
+import LoadingSpinner from './assets/loading-spinner.gif';
+
+
+export function ChatInput({ chatMessages, setChatMessages }) {
+  
+    const [inputText, setInputText] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    function saveInputText(event) {
+        setInputText(event.target.value);
+    }
+
+  async function sendMessage() {
+    if (isLoading || inputText === '') {
+      return;
+    }
+    setIsLoading(true);
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: 'User',
+        id: crypto.randomUUID()
+      }
+    ];
+
+    setInputText('');
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: <img src={LoadingSpinner} className="loading-spinner"/>,
+        sender: 'Robot',
+        id: crypto.randomUUID()
+      }
+    ]);
+    const response = await (Chatbot.getResponseAsync(inputText));
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: response,
+        sender: 'Robot',
+        id: crypto.randomUUID()
+      }
+    ]);
+    setIsLoading(false);
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      sendMessage();
+    } else if (event.key === 'Escape') {
+      setInputText('');
+    }
+  }
+
+  return (
+    <div className="input-area-container">
+      <input
+        className="input-area"
+        placeholder="Send a message to Chatbot"
+        size="30"
+        onChange={saveInputText}
+        onKeyDown={handleKeyDown}
+        value={inputText} // Controlled Input
+      />
+      <button
+        className="send-button"
+        onClick={sendMessage}
+      >Send</button>
+    </div>
+  );
+}
