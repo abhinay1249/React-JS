15b310509e7a50c0c8fe3a24cb20885797ff9259
abhinay1249 <abhinaymarise@gmail.com>
2026-01-15 17:38:00 +0530

    React-Router

--- ecommerce-Project/src/App.jsx
+++ ecommerce-Project/src/App.jsx
@@ -6,7 +6,8 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>}></Route>
+      <Route index element={<HomePage/>}/>
+      <Route path="/checkout" element={<Checkout/>}/>
     </Routes>
 
   )
