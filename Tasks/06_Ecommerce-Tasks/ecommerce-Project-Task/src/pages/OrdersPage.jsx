673d18554726941c94b81b2306799f02a026a868
abhinay1249 <abhinaymarise@gmail.com>
2026-01-16 22:54:52 +0530

    Initial Commit

--- Tasks/06_Ecommerce-Tasks/ecommerce-Project-Task/src/pages/OrdersPage.jsx
+++ Tasks/06_Ecommerce-Tasks/ecommerce-Project-Task/src/pages/OrdersPage.jsx
@@ -1,4 +1,5 @@
 import { Header } from '../components/Header';
+import BuyAgainIcon from '../assets/images/icons/buy-again.png';
 import './OrdersPage.css';
 
 export function OrdersPage() {
@@ -47,7 +48,7 @@ export function OrdersPage() {
                   Quantity: 1
                 </div>
                 <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                  <img className="buy-again-icon" src={BuyAgainIcon} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
@@ -75,7 +76,7 @@ export function OrdersPage() {
                   Quantity: 2
                 </div>
                 <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                  <img className="buy-again-icon" src={BuyAgainIcon} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
@@ -126,7 +127,7 @@ export function OrdersPage() {
                   Quantity: 2
                 </div>
                 <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                  <img className="buy-again-icon" src={BuyAgainIcon} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
