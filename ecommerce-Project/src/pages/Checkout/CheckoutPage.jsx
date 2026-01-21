8ae5d53712f7483f76dd003008e04d72621a9c8c
abhinay1249 <abhinaymarise@gmail.com>
2026-01-21 21:56:55 +0530

    Split Pages

--- ecommerce-Project/src/pages/Checkout/CheckoutPage.jsx
+++ ecommerce-Project/src/pages/Checkout/CheckoutPage.jsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { CheckoutHeader } from './CheckoutHeader';
 import { formatMoney } from '../../utils/money';
-
 import './CheckoutPage.css';
 import { OrderSummary } from './OrderSummary';
 
@@ -35,43 +34,44 @@ export function CheckoutPage({ cart }) {
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
 
-        <div className="checkout-grid">
-          <OrderSummary deliveryOptions={deliveryOptions} cart={cart}/>
-          {paymentSummary && (
-            <div className="payment-summary">
-              <div className="payment-summary-title">
-                Payment Summary
-              </div>
-
-              <div className="payment-summary-row">
-                <div>Items ({paymentSummary.totalItems}):</div>
-                <div className="payment-summary-money">{formatMoney(paymentSummary.productCostCents)}</div>
-              </div>
-
-              <div className="payment-summary-row">
-                <div>Shipping &amp; handling:</div>
-                <div className="payment-summary-money">{formatMoney(paymentSummary.shippingCostCents)}</div>
-              </div>
-
-              <div className="payment-summary-row subtotal-row">
-                <div>Total before tax:</div>
-                <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostBeforeTaxCents)}</div>
-              </div>
-
-              <div className="payment-summary-row">
-                <div>Estimated tax (10%):</div>
-                <div className="payment-summary-money">{formatMoney(paymentSummary.taxCents)}</div>
-              </div>
-
-              <div className="payment-summary-row total-row">
-                <div>Order total:</div>
-                <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostCents)}</div>
-              </div>
-
-              <button className="place-order-button button-primary">
-                Place your order
-              </button>
+          <div className="checkout-grid">
+            <OrderSummary deliveryOptions={deliveryOptions} cart={cart} />
+             
+          <div className="payment-summary">
+            <div className="payment-summary-title">
+              Payment Summary
             </div>
+            {paymentSummary && (
+              <>
+                <div className="payment-summary-row">
+                  <div>Items ({paymentSummary.totalItems}):</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.productCostCents)}</div>
+                </div>
+
+                <div className="payment-summary-row">
+                  <div>Shipping &amp; handling:</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.shippingCostCents)}</div>
+                </div>
+
+                <div className="payment-summary-row subtotal-row">
+                  <div>Total before tax:</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostBeforeTaxCents)}</div>
+                </div>
+
+                <div className="payment-summary-row">
+                  <div>Estimated tax (10%):</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.taxCents)}</div>
+                </div>
+
+                <div className="payment-summary-row total-row">
+                  <div>Order total:</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostCents)}</div>
+                </div>
+
+                <button className="place-order-button button-primary">
+                  Place your order
+                </button>
+              </>
           )}
         </div>
       </div>
--- ecommerce-Project/src/pages/Checkout/OrderSummary.jsx
+++ ecommerce-Project/src/pages/Checkout/OrderSummary.jsx
@@ -6,18 +6,16 @@ export function OrderSummary([deliveryOptions, cart]) {
         <div className="order-summary">
 
             {deliveryOptions.length > 0 && cart.map((cartItem) => {
-
                 const selectedDeliveryOption = deliveryOptions
                     .find((deliveryOption) => {
                         return deliveryOption.id === cartItem.deliveryOptionId;
                     });
-
                 return (
                     <div key={cartItem.productId} className="cart-item-container">
                         <div className="delivery-date">
                             Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
                         </div>
-
+                        
                         <div className="cart-item-details-grid">
                             <img className="product-image"
                                 src={cartItem.product.image} />
