1ef5b4ecf0c480536387fa5c3132ae3ab1cc0289
abhinay1249 <abhinaymarise@gmail.com>
2026-01-21 22:46:49 +0530

    Split Pages Into Sections

--- ecommerce-Project/src/pages/Checkout/DeliveryOptions.jsx
+++ ecommerce-Project/src/pages/Checkout/DeliveryOptions.jsx
@@ -34,5 +34,5 @@ export function DeliveryOptions({ deliveryOptions, cartItem }) {
             })}
 
         </div>
-    )
+    );
 }
--- ecommerce-Project/src/pages/Checkout/OrderSummary.jsx
+++ ecommerce-Project/src/pages/Checkout/OrderSummary.jsx
@@ -49,5 +49,5 @@ export function OrderSummary({deliveryOptions, cart}) {
             }
 
         </div>
-    )
+    );
 }
