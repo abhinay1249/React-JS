150b22abceed4e7a60d5d516bd1bfd4dffc11a79
abhinay1249 <abhinaymarise@gmail.com>
2026-01-21 23:21:02 +0530

    Async Await

--- ecommerce-Project/src/pages/home/HomePage.jsx
+++ ecommerce-Project/src/pages/home/HomePage.jsx
@@ -11,10 +11,13 @@ export function HomePage({ cart }) {
   
 
   useEffect(() => {
-    axios.get("/api/products")
-      .then((response) => {
-        setProducts(response.data);
-      });
+    const getHomeData =  async () => {
+      const response = await axios.get("/api/products")
+      setProducts(response.data);
+    }
+    
+    getHomeData();
+
   },[]); //[] is the dependency array used to run only once
 
 
