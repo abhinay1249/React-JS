4887c147480a7938f41e26f94e3cf5c7fa218b15
abhinay1249 <abhinaymarise@gmail.com>
2026-01-22 23:14:23 +0530

    Task-7-6 Completed

--- Tasks/07_BackEnd-Data-Fetching-Async-Await/ecommerce-Project/src/pages/Orders/OrdersGrid.jsx
+++ Tasks/07_BackEnd-Data-Fetching-Async-Await/ecommerce-Project/src/pages/Orders/OrdersGrid.jsx
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 import { formatMoney } from "../../utils/money";
 import BuyAgainIcon from '../../assets/images/icons/buy-again.png';
 import { Fragment } from "react";
+import { OrdersHeader } from "./OrdersHeader";
 
 export function OrdersGrid({ orders }) {
     return (
@@ -10,24 +11,8 @@ export function OrdersGrid({ orders }) {
             {orders.map((order) => {
                 return (
                     <div className="order-container">
-
-                        <div className="order-header">
-                            <div className="order-header-left-section">
-                                <div className="order-date">
-                                    <div className="order-header-label">Order Placed:</div>
-                                    <div>{dayjs(order.orderTimeMs).format('MMMM D')}</div>
-                                </div>
-                                <div className="order-total">
-                                    <div className="order-header-label">Total:</div>
-                                    <div>{formatMoney(order.totalCostCents)}</div>
-                                </div>
-                            </div>
-
-                            <div className="order-header-right-section">
-                                <div className="order-header-label">Order ID:</div>
-                                <div>{order.id}</div>
-                            </div>
-                        </div>
+                        
+                        <OrdersHeader order={order}/>
 
                         <div className="order-details-grid">
                             {order.products.map((orderProduct) => {
