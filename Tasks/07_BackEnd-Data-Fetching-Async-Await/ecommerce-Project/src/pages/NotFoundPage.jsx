d2110b9711025b83f53f6f85ed8469e79bfece11
abhinay1249 <abhinaymarise@gmail.com>
2026-02-08 20:56:53 +0530

    Tasks 7-15

--- Tasks/07_BackEnd-Data-Fetching-Async-Await/ecommerce-Project/src/pages/NotFoundPage.jsx
+++ Tasks/07_BackEnd-Data-Fetching-Async-Await/ecommerce-Project/src/pages/NotFoundPage.jsx
@@ -1,13 +1,13 @@
 import {Header} from '../components/Header';
 import './NotFoundPage.css';
 
-export function NotFoundPage(){
+export function NotFoundPage({cart}){
     return(
         <>
             <title>404 Page Not Found</title>
             <link rel="icon" type="image/svg+xml" href="/vite.svg" />
 
-            <Header/>
+            <Header cart={cart}/>
 
             <div className='not-found-message'>
                 <h1>Page Not Found</h1>
