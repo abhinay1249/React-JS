ef34f59d03533099d2e9551a2c5b0eca6b85feb9
abhinay1249 <abhinaymarise@gmail.com>
2026-02-08 19:51:49 +0530

    File Spacing

--- Tasks/07_BackEnd-Data-Fetching-Async-Await/ecommerce-Project/src/pages/Orders/OrdersGrid.jsx
+++ Tasks/07_BackEnd-Data-Fetching-Async-Await/ecommerce-Project/src/pages/Orders/OrdersGrid.jsx
@@ -4,53 +4,53 @@ import { Fragment } from "react";
 import { OrdersHeader } from "./OrdersHeader";
 
 export function OrdersGrid({ orders }) {
-    return (
-        <div className="orders-grid">
+  return (
+    <div className="orders-grid">
 
-            {orders.map((order) => {
+      {orders.map((order) => {
+        return (
+          <div className="order-container">
+
+            <OrdersHeader order={order} />
+
+            <div className="order-details-grid">
+              {order.products.map((orderProduct) => {
                 return (
-                    <div className="order-container">
-                        
-                        <OrdersHeader order={order}/>
-
-                        <div className="order-details-grid">
-                            {order.products.map((orderProduct) => {
-                                return (
-                                    <Fragment key={orderProduct.product.id}>
-                                        <div className="product-image-container">
-                                            <img src={orderProduct.product.image} />
-                                        </div>
-
-                                        <div className="product-details">
-                                            <div className="product-name">
-                                                {orderProduct.product.name}
-                                            </div>
-                                            <div className="product-delivery-date">
-                                                Arriving on: {dayjs(orderProduct.product.estimatedDeliveryTimeMs).format('MMMM D')}
-                                            </div>
-                                            <div className="product-quantity">
-                                                Quantity: {orderProduct.quantity}
-                                            </div>
-                                            <button className="buy-again-button button-primary">
-                                                <img className="buy-again-icon" src={BuyAgainIcon} />
-                                                <span className="buy-again-message">Add to Cart</span>
-                                            </button>
-                                        </div>
-
-                                        <div className="product-actions">
-                                            <a href={`/tracking/${order.id}/${orderProduct.product.id}`}>
-                                                <button className="track-package-button button-secondary">
-                                                    Track package
-                                                </button>
-                                            </a>
-                                        </div>
-                                    </Fragment>
-                                )
-                            })}
-                        </div>
+                  <Fragment key={orderProduct.product.id}>
+                    <div className="product-image-container">
+                      <img src={orderProduct.product.image} />
                     </div>
-                );
-            })}
-        </div>
-    )
+
+                    <div className="product-details">
+                      <div className="product-name">
+                        {orderProduct.product.name}
+                      </div>
+                      <div className="product-delivery-date">
+                        Arriving on: {dayjs(orderProduct.product.estimatedDeliveryTimeMs).format('MMMM D')}
+                      </div>
+                      <div className="product-quantity">
+                        Quantity: {orderProduct.quantity}
+                      </div>
+                      <button className="buy-again-button button-primary">
+                        <img className="buy-again-icon" src={BuyAgainIcon} />
+                        <span className="buy-again-message">Add to Cart</span>
+                      </button>
+                    </div>
+
+                    <div className="product-actions">
+                      <a href={`/tracking/${order.id}/${orderProduct.product.id}`}>
+                        <button className="track-package-button button-secondary">
+                          Track package
+                        </button>
+                      </a>
+                    </div>
+                  </Fragment>
+                )
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  )
 }
