45cd55660a31a2adfb7cfc7ab88494a8271373d7
abhinay1249 <abhinaymarise@gmail.com>
2026-02-14 23:52:28 +0530

    Data Mutatation-Cleanup

--- ecommerce-Project/src/pages/home/Product.jsx
+++ ecommerce-Project/src/pages/home/Product.jsx
@@ -7,6 +7,14 @@ export function Product({product, loadCart}) {
 
     const [quantity, setQuantity] = useState(1);
 
+    const addToCart = async () => {
+                    await axios.post('/api/cart-items', {
+                        productId: product.id,
+                        quantity: quantity
+                    });
+                    await loadCart();
+    }
+
     return (
         <div className="product-container">
             <div className="product-image-container">
@@ -57,13 +65,7 @@ export function Product({product, loadCart}) {
             </div>
 
             <button className="add-to-cart-button button-primary"
-                onClick={async () => {
-                    await axios.post('/api/cart-items', {
-                        productId: product.id,
-                        quantity: quantity
-                    });
-                    await loadCart();
-                }}>
+                onClick={addToCart}>
                 Add to Cart
             </button>
         </div>
