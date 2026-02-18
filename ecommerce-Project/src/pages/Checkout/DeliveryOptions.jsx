ef2198123a83eb411f9f48d89adb924cbfb9fbc2
abhinay1249 <abhinaymarise@gmail.com>
2026-02-18 23:51:23 +0530

    Delivery Options Update Completed

--- ecommerce-Project/src/pages/Checkout/DeliveryOptions.jsx
+++ ecommerce-Project/src/pages/Checkout/DeliveryOptions.jsx
@@ -2,7 +2,7 @@ import { formatMoney } from "../../utils/money";
 import axios from 'axios';
 import dayjs from "dayjs";
 
-export function DeliveryOptions({ deliveryOptions, cartItem }) {
+export function DeliveryOptions({ deliveryOptions, cartItem, loadCart }) {
     return (
         <div className="delivery-options">
             <div className="delivery-options-title">
@@ -20,11 +20,12 @@ export function DeliveryOptions({ deliveryOptions, cartItem }) {
                     await axios.put(`/api/cart-items/${cartItem.productId}`,{
                         deliveryOptionId:deliveryOption.id
                     });
+                    await loadCart();
                 }
 
 
                 return (
-                    <div key={deliveryOption.id} className="delivery-option" onClick={}>
+                    <div key={deliveryOption.id} className="delivery-option" onClick={updateDeliveryOption}>
                         <input type="radio"
                             checked={deliveryOption.id === cartItem.deliveryOptionId}
                             className="delivery-option-input"
