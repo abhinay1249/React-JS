e3d49c1576b96715281db176e137603db847f32b
abhinay1249 <abhinaymarise@gmail.com>
2026-03-15 20:51:10 +0530

    Tasks-08-T2

--- Tasks/08_Data_Mutation_Types_Of_Requests/ecommerce-Project/src/pages/Checkout/CheckoutPage.jsx
+++ Tasks/08_Data_Mutation_Types_Of_Requests/ecommerce-Project/src/pages/Checkout/CheckoutPage.jsx
@@ -8,18 +8,28 @@ import './CheckoutPage.css';
 export function CheckoutPage({ cart, loadCart }) {
 
   const [deliveryOptions, setDeliveryOptions] = useState([]);
-  const [paymentSummary, setPaymentSummary] = useState(null);
+  const [paymentSummary, setPaymentSummary] = useState([null]);
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
-      let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
+      
+      const response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
       setDeliveryOptions(response.data);
-
-      response = await axios.get('/api/payment-summary')
-      setPaymentSummary(response.data);
+    
     };
     fetchCheckoutData();
-  }, [cart]);
+  }, []);
+
+  useEffect(()=>{
+    const fetchPaymentSummary = async () => {
+
+      const response = await axios.get('/api/payment-summary')
+      setPaymentSummary(response.data);
+
+    };
+    fetchPaymentSummary();
+
+  },[cart])
 
 
   return (
