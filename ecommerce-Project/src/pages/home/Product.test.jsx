5a290b52f023664c74e7a870dbf1baf7092dd638
abhinay1249 <abhinaymarise@gmail.com>
2026-03-25 08:12:31 +0530

    Integration Testing Setup

--- ecommerce-Project/src/pages/home/Product.test.jsx
+++ ecommerce-Project/src/pages/home/Product.test.jsx
@@ -1,9 +1,21 @@
-import {describe, expect, it} from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { render } from '@testing-library/react';
 import { Product } from './Product';
 
-describe('Product Component', ()=>{
-    it('displays the product details correctly', ()=>{
-        expect(Product).toBe();
+describe('Product Component', () => {
+    it('displays the product details correctly', () => {
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+                stars: 4.5,
+                count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        }
+        expect(render(<Product product={product} />)).toBe();
     })
 })
 
