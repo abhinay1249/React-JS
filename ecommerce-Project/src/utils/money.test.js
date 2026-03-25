319c795f402c2abe51349548e1d51bf3ee189068
abhinay1249 <abhinaymarise@gmail.com>
2026-03-25 08:00:12 +0530

    Unit Testing

--- ecommerce-Project/src/utils/money.test.js
+++ ecommerce-Project/src/utils/money.test.js
@@ -1,10 +1,12 @@
-import {expect, it} from 'vitest';
+import {describe, expect, it} from 'vitest';
 import { formatMoney } from './money';
 
-it('Format Money 1090 cents into $10.90', () => {
-    expect(formatMoney(1090)).toBe('$10.90');
-});
+describe('Format Money',()=>{
+    it('Format Money 1090 cents into $10.90', () => {
+        expect(formatMoney(1090)).toBe('$10.90');
+    });
 
-it('Format Money 90149 cents Into $90.149',()=>{
-    expect(formatMoney(9014)).toBe('$90.14');
+    it('Format Money 90149 cents Into $90.149',()=>{
+        expect(formatMoney(9014)).toBe('$90.14');
+    })
 })
