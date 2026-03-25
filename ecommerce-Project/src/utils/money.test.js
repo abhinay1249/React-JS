591a604b6c68e2dd8210484d6cf5a55749bf0e63
abhinay1249 <abhinaymarise@gmail.com>
2026-03-25 07:58:50 +0530

    Automated Testing

---
+++ ecommerce-Project/src/utils/money.test.js
@@ -0,0 +1,10 @@
+import {expect, it} from 'vitest';
+import { formatMoney } from './money';
+
+it('Format Money 1090 cents into $10.90', () => {
+    expect(formatMoney(1090)).toBe('$10.90');
+});
+
+it('Format Money 90149 cents Into $90.149',()=>{
+    expect(formatMoney(9014)).toBe('$90.14');
+})
