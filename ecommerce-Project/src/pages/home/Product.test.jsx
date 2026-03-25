a41f70e25d0a1b0d4f98132b3a6aaea0a163998f
abhinay1249 <abhinaymarise@gmail.com>
2026-03-25 08:07:03 +0530

    Integration Testing

---
+++ ecommerce-Project/src/pages/home/Product.test.jsx
@@ -0,0 +1,9 @@
+import {describe, expect, it} from 'vitest';
+import { Product } from './Product';
+
+describe('Product Component', ()=>{
+    it('displays the product details correctly', ()=>{
+        expect(Product).toBe();
+    })
+})
+
