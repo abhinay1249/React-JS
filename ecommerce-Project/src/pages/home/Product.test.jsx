0e4161aa22eb0bd8622acefb1aa94d4555003d7e
abhinay1249 <abhinaymarise@gmail.com>
2026-03-26 22:54:57 +0530

    Test User Interactions

--- ecommerce-Project/src/pages/home/Product.test.jsx
+++ ecommerce-Project/src/pages/home/Product.test.jsx
@@ -19,34 +19,55 @@ describe('Product Component', () => {
 
         const loadCart = vi.fn();
 
-        render(<Product product={product} loadCart={loadCart}  />);
+        render(<Product product={product} loadCart={loadCart} />);
 
-    expect(
-        screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')
-    ).toBeInTheDocument();
+        expect(
+            screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')
+        ).toBeInTheDocument();
 
-    expect(
-        screen.getByText('$10.90')
-    ).toBeInTheDocument();
+        expect(
+            screen.getByText('$10.90')
+        ).toBeInTheDocument();
 
-    expect(
-        screen.getByTestId('product-image')
-    ).toHaveAttribute('src','images/products/athletic-cotton-socks-6-pairs.jpg');
+        expect(
+            screen.getByTestId('product-image')
+        ).toHaveAttribute('src', 'images/products/athletic-cotton-socks-6-pairs.jpg');
 
-    expect(
-        screen.getByTestId("product-rating-stars")
-    ).toHaveAttribute('src','images/ratings/rating-45.png');
+        expect(
+            screen.getByTestId("product-rating-stars")
+        ).toHaveAttribute('src', 'images/ratings/rating-45.png');
 
-    expect(
-        screen.getByText('87')
-    ).toBeInTheDocument();
+        expect(
+            screen.getByText('87')
+        ).toBeInTheDocument();
 
     });
-    
-    it('add product to the cart',()=>{
+
+    it('add product to the cart', async () => {
+
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+                stars: 4.5,
+                count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        }
+
+        const loadCart = vi.fn();
+
+        render(<Product product={product} loadCart={loadCart} />);
+
         const user = userEvent.setup()
+        const addToCartButton = screen.getByTestId('add-to-cart-button');
+        await user.click(addToCartButton);
+
+
     })
-    
+
 
 });
 
