0d03a7f4f6298a3bfe279b12f0cf5aa5e11c7772
abhinay1249 <abhinaymarise@gmail.com>
2026-04-12 23:07:35 +0530

    TESTS

--- ecommerce-Project/src/pages/home/Product.test.jsx
+++ ecommerce-Project/src/pages/home/Product.test.jsx
@@ -25,13 +25,11 @@ describe('Product Component', () => {
                 priceCents: 1090,
                 keywords: ["socks", "sports", "apparel"]
             }
+            loadCart = vi.fn();
         });
 
-        loadCart = vi.fn();
+
     it('displays the product details correctly', () => {
-
-
-        
 
         render(<Product product={product} loadCart={loadCart} />);
 
@@ -58,20 +56,6 @@ describe('Product Component', () => {
     });
 
     it('add product to the cart', async () => {
-
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        }
-
-        const loadCart = vi.fn();
 
         render(<Product product={product} loadCart={loadCart} />);
 
