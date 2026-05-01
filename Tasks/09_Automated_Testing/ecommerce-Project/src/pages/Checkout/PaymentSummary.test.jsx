c4355d411fb22c418c4112572b20eb07dfaeb7b2
abhinay1249 <abhinaymarise@gmail.com>
2026-05-01 17:52:28 +0530

    Automated-Testing-Tasks-10

--- Tasks/09_Automated_Testing/ecommerce-Project/src/pages/Checkout/PaymentSummary.test.jsx
+++ Tasks/09_Automated_Testing/ecommerce-Project/src/pages/Checkout/PaymentSummary.test.jsx
@@ -1,13 +1,19 @@
 import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter, useLocation } from 'react-router';
 import { PaymentSummary } from './PaymentSummary';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
+
+vi.mock('axios');
 
 
 describe('Payment Summary Component',()=>{
     let paymentSummary;
-    
+
     let loadCart;
+
+    let user;
 
     beforeEach(()=>{
         paymentSummary = {
@@ -19,6 +25,7 @@ describe('Payment Summary Component',()=>{
             totalCostCents: 5251           
         };
         loadCart = vi.fn();
+        user = userEvent.setup();
     });
 
     it('displays the correct details', async() =>{
@@ -64,4 +71,28 @@ describe('Payment Summary Component',()=>{
     ).toHaveTextContent('$52.51');
     
     });
-})
+
+    it('places an order',async()=>{
+        function Location(){
+            const location = useLocation();
+            return <div data-testid = "url-path">{location.pathname}</div>
+        }
+        render(
+            <MemoryRouter>
+                <PaymentSummary
+                    paymentSummary={paymentSummary}
+                    loadCart={loadCart}
+                />
+                <Location/>
+            </MemoryRouter>
+        );
+
+        const placeOrderButton = screen.getByTestId('place-order-button');
+
+        await user.click(placeOrderButton);
+
+        expect(axios.post).toHaveBeenCalledWith('/api/orders');
+        expect(loadCart).toHaveBeenCalled();
+        expect(screen.getByTestId('url-path')).toHaveTextContent('/orders');
+    });
+});
