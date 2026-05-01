f1722f8177f4aa5540f3f6b58a61334e3dece266
abhinay1249 <abhinaymarise@gmail.com>
2026-05-01 17:22:59 +0530

    Automated-Testing-Tasks-9

---
+++ Tasks/09_Automated_Testing/ecommerce-Project/src/pages/Checkout/PaymentSummary.test.jsx
@@ -0,0 +1,5 @@
+import { it, expect, describe, vi, beforeEach } from 'vitest';
+import { render, screen, within } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import { PaymentSummary } from './PaymentSummary';
+
