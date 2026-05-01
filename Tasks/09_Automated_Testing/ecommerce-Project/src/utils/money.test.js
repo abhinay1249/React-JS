754206fe2569b44180315fdbffc5f70e7d42d285
abhinay1249 <abhinaymarise@gmail.com>
2026-05-01 13:18:40 +0530

    Automated-Testing-Tasks-2

--- Tasks/09_Automated_Testing/ecommerce-Project/src/utils/money.test.js
+++ Tasks/09_Automated_Testing/ecommerce-Project/src/utils/money.test.js
@@ -2,7 +2,11 @@ import {describe, it, expect} from 'vitest';
 import {formatMoney} from './money';
 
 describe('Format Money',()=>{
-    it('Format Money 0 cents to $0.00',()=>{
-        expect(formatMoney(0)).toBe('$0.00');
-    });
+    // it('Format Money 0 cents to $0.00',()=>{
+    //     expect(formatMoney(0)).toBe('$0.00');
+    // });
+    it('Format Money -999 cents to -$9.99',()=>{
+        expect(formatMoney(-999)).toBe('-$9.99');
+        expect(formatMoney(-100)).toBe('-$1.00');
+    })
 })
